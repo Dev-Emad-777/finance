691e9d1668261ad6e72767e3ea8e851a485d99e1
Dev-Emad-777 <emadezi777@gmail.com>
2024-08-13 19:37:12 +0300

    debug(avatars)

--- app.py
+++ app.py
@@ -4,11 +4,10 @@ from cs50 import SQL
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
-
+from helpers import apology, login_required, lookup, usd
 from flask_avatars import Avatars
 
 
-from helpers import apology, login_required, lookup, usd
 
 # Configure application
 app = Flask(__name__)
